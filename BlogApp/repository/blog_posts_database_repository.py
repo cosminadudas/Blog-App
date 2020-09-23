d82124c8e74e2e8db806bf07a8c5104524775a72
cosminadudas <cosminadudas@users.noreply.github.com>
2020-09-23 22:13:29 +0300

    implemented add, edit and delete methods in database repository

--- BlogApp/repository/blog_posts_database_repository.py
+++ BlogApp/repository/blog_posts_database_repository.py
@@ -1,22 +1,37 @@
 import psycopg2
+from models.BlogPost import BlogPost
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="blog",
-    user="postgres",
-    password="postgres")
 
-cur = conn.cursor()
-cur.execute("""
-CREATE TABLE posts
-(
-ID INT NOT NULL,
-OWNER TEXT NOT NULL,
-TITLE TEXT NOT NULL,
-CONTENT TEXT NOT NULL
-)
-"""
-)
+class Posts:
 
-conn.commit()
-conn.close()
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="localhost",
+            database="blog",
+            user="postgres",
+            password="postgres")
+        self.cur = conn.cursor()
+        self.cur.execute("""
+        CREATE TABLE posts
+            (
+                ID INT NOT NULL,
+                OWNER TEXT NOT NULL,
+                TITLE TEXT NOT NULL,
+                CONTENT TEXT NOT NULL
+            )
+        """
+        )
+
+    def add_post(self, new_post:BlogPost):
+        self.cur.execute("INSERT INTO posts (ID, OWNER, TITLE, CONTENT) VALUES(new_post.id, new_post.owner, new_post.title, new_post.content)")
+        self.conn.commit()
+        self.conn.close()
+
+
+    def edit(self, post_id, new_title, new_content):
+        self.cur.execute("UPDATE posts SET TITLE = new_title AND CONTENT = new_content WHERE ID = post_id")
+        self.conn.commit()
+        self.conn.close()
+
+    def delete(self, post_id):
+        self.cur.execute("DELETE FROM posts WHERE ID = post_id")
