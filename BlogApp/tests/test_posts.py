db3639dd2148681f99ea4684a976ce8789c68adb
cosminadudas <cosminadudas@users.noreply.github.com>
2020-10-01 14:07:33 +0300

    edited .pylintrc file and resolved errors

--- BlogApp/tests/test_posts.py
+++ BlogApp/tests/test_posts.py
@@ -28,14 +28,17 @@ def test_add_post_route(client):
     response = client.post('/add', data=dict(
         title="Post 3",
         owner="Cosmina",
-        content="This is the third post"),follow_redirects=True)
+        content="This is the third post"), follow_redirects=True)
     assert b'Cosmina' in response.data
     assert b'Post 3' in response.data
 
 
 def test_edit_post_route(client):
     assert client.get('/edit/2', follow_redirects=True).status_code == 200
-    response = client.post('/edit/2', data=dict(title='updated', content='This is the second post'), follow_redirects=True)
+    response = client.post('/edit/2',
+                           data=dict(title='updated',
+                                     content='This is the second post'),
+                           follow_redirects=True)
     with app.test_client() as client:
         client.get('/?post_id=2', follow_redirects=True)
         assert request.args['post_id'] == '2'
@@ -49,7 +52,8 @@ def test_delete_post_route(client):
 
 def test_view_post_route(client):
     assert client.get('/edit/1', follow_redirects=True).status_code == 200
-    client.post('/edit/1', data=dict(content='This is the first post updated'), follow_redirects=True)
+    client.post('/edit/1',
+                data=dict(content='This is the first post updated'), follow_redirects=True)
     with app.test_client() as client:
         client.get('/?post_id=1', follow_redirects=True)
         assert request.args['post_id'] == '1'
