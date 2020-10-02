353ef56174ed1e24efd9d734a0a432e282002510
cosminadudas <cosminadudas@users.noreply.github.com>
2020-10-02 11:30:21 +0300

    refactored

--- BlogApp/views/setup.py
+++ BlogApp/views/setup.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint, request, redirect, render_template
 from database_setup.database import Database
 
@@ -6,13 +7,14 @@ database_setup = Database()
 
 @setup_blueprint.route('/setup', methods=["GET", "POST"])
 def db_setup():
-    if database_setup.is_set_up():
+    if os.path.exists('database_setup/database.ini'):
         return redirect('/home')
     if request.method == "POST":
         user = request.form['user']
         password = request.form['password']
         database = request.form['database']
         database_setup.credentials.save_credentials(user, password, database)
+        database_setup.create_database(database, user, password)
         return redirect('/home')
     return render_template('setup_db.html')
     
