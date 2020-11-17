eba3229d5710ad2f7ce62a5a3fcb2518789b6d5f
cosminadudas <cosminadudas@users.noreply.github.com>
2020-11-17 13:24:30 +0200

    refactored api route and passed all tests

--- BlogApp/tests/test_api.py
+++ BlogApp/tests/test_api.py
@@ -6,3 +6,7 @@ def test_api_route_when_config_file_exists(is_config):
 def test_api_route_when_config_file_does_not_exist(is_not_config):
     result = is_not_config.get('/API/post/1', follow_redirects=True)
     assert b'User' in result.data
+
+def test_api_route_when_config_file_exists_and_id_not_exist(is_config):
+    result = is_config.get('/API/post/25')
+    assert b'404' in result.data
--- BlogApp/views/api.py
+++ BlogApp/views/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort
 from injector import inject
 from views.decorators.setup_required import setup_required
 from repository.blog_posts_interface import BlogPostsInterface
@@ -11,6 +11,8 @@ api_blueprint = Blueprint("api_blueprint", __name__)
 @setup_required
 def api_post_view(blog_posts: BlogPostsInterface, post_id):
     post = blog_posts.get_post_by_id(post_id)
+    if post is None:
+        return abort(404)
     return jsonify({
         'title': post.title,
         'owner': post.owner,
